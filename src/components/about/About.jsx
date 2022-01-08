fcb4c260c9e3a07ded93ec7e05e136b362beff3d
PhotoshopLOL <joshphotoshop@gmail.com>
2022-01-09 01:28:32 +0800

    contact section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,31 @@
 import "./about.scss"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookMessenger, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 export default function About() {
     return (
         <div className='contact' id="contact">
-            Contact me
+            <div className="aboutLeft">
+                <div className="imgContainer">
+                    <img src="josh-photo-field.png" alt="" />
+                </div>
+            </div>
+            <div className="aboutRight">
+                <h2> Hello! I'm Josh.</h2>
+
+                <span> I am an aspiring documentary filmmaker, web developer, and student.</span>
+                
+                <div className="socialMedia">
+                    <ul>
+                        <li><FontAwesomeIcon icon={faEnvelope}/> <span>josh.mutia@benilde.edu.ph</span></li>
+                        <li><FontAwesomeIcon icon={faFacebookMessenger}/> <span>fb.me/photoshoplolpersonal</span></li>
+                        <li><FontAwesomeIcon icon={faLinkedin}/> <span>linkedin.com/in/joshmutia</span></li>
+                    </ul>
+                </div>
+            </div>
+            
         </div>
     )
 }
