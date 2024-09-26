5153958364e6f43a4aefc017d6a908034ec917da
Josh Mutia <joshphotoshop@gmail.com>
2024-09-26 19:19:03 +0800

    feat: cleanup + add teaser to /dev

--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -1,9 +1,17 @@
+<script>
+
+    import { popup } from "@skeletonlabs/skeleton";
+
+</script>
 <style>
 
 </style>
 
-<div class="container mx-auto">
+<div class="container mx-auto flex flex-col">
     <div class="h1 text-center mt-10">WORK IN PROGRESS</div>
     <div class="h1 text-center mt-10">🏗️🚧🚧🚧🛠️</div>
+
+    <div data-popup="websitehint" class="card p-2.5 mt-2.5 w-fit shadow-xl variant-filled-success">me 😎</div>
+    <button class="text-xs self-center mt-5" use:popup={{event: 'click', target: 'websitehint', placement: 'bottom'}} >who made this website?</button>
 
 </div>
